821702837df3b3a324e3388f832287bfe7a4943d
Hannah Neirman <hneirman@gmail.com>
2020-04-07 01:53:30 +0000

    loops

---
+++ main.py
@@ -0,0 +1,28 @@
+user_age = int(input("please enter your age: "))
+print ("Your age is {}". format(user_age))
+
+if user_age > 18:
+  print("You are old")
+else:
+    print("You are young")
+    
+# Using for loop to display numbers
+print ("For Loop")
+for i in range(0,5):
+   print (i)
+   
+# Using WHILE loop to display numbers
+print ("While Loop")
+i-0
+while i < 5:
+   print(i)
+   i=i+1
+   
+print ("For Loop")
+for i in range(0,5):
+   print ("Rose")
+   
+value = int(input("please enter a value to count towards: "))
+
+for i in range(0,value):
+  print(i)
